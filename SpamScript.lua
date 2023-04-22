ad50a58d49f6b9b59485d5a84f8d4f522c55d5ed
BarisSenel <122669318+BarisSenel@users.noreply.github.com>
2023-04-22 21:56:25 +0300

    Update SpamScript.lua

--- SpamScript.lua
+++ SpamScript.lua
@@ -1,14 +1,16 @@
 while true do
-spamtext = "HELLO"
+spamtext = "HELLO"   
 botpositionx = 37 - 1   
 botpositiony = 21 - 1   
+
+
 
 if getBot().x == botpositionx * 32 and getBot().y == botpositiony * 32 then
 say(spamtext)
 sleep(10000)
 
 else
-findPath(36,20)
+findPath(botpositionx,botpositiony)
 sleep(10000)
 say(spamtext)
 end
