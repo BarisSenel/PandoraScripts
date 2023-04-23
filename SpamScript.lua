f95f3d0cb7c88656ad82c13896e9b0b50e4e634f
BarisSenel <122669318+BarisSenel@users.noreply.github.com>
2023-04-23 09:44:06 +0300

    Update SpamScript.lua

--- SpamScript.lua
+++ SpamScript.lua
@@ -2,6 +2,12 @@ while true do
 spamtext = "HELLO"   
 botpositionx = 37 - 1   
 botpositiony = 21 - 1   
+SpamWorld = "DESIREDWORLD"
+
+if not getBot().world == SpamWorld then
+
+    sendPacket(3,"action|join_request\nname|SpamWorld\ninvitedWorld|0")
+end
 
 
 
@@ -15,5 +21,6 @@ sleep(10000)
 say(spamtext)
 end
 end
+
 
 
