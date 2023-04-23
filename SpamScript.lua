974b1f2d8808ba81b3b7559ea69a96016c6f750e
BarisSenel <122669318+BarisSenel@users.noreply.github.com>
2023-04-24 00:27:17 +0300

    Update SpamScript.lua

--- SpamScript.lua
+++ SpamScript.lua
@@ -1,3 +1,5 @@
+
+
 while true do
 spamtext = "HELLO"   
 botpositionx = 37 - 1   
@@ -5,22 +7,33 @@ botpositiony = 21 - 1
 SpamWorld = "DESIREDWORLD"
 
 if not getBot().world == SpamWorld then
-
+    sleep(50000) --try to rejoin after 50 second you can adjust that 
     sendPacket(3,"action|join_request\nname|SpamWorld\ninvitedWorld|0")
+
 end
 
 
 
 if getBot().x == botpositionx * 32 and getBot().y == botpositiony * 32 then
 say(spamtext)
-sleep(10000)
+sleep(10000) --spam speed
 
 else
 findPath(botpositionx,botpositiony)
-sleep(10000)
+sleep(10000) --spam speed
 say(spamtext)
 end
 end
+
+
+
+
+
+
+
+
+
+
 
 
 
