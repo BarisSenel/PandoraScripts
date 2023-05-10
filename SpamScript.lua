a8d457a8d1aedb52f2e0c63610148a1cf8725d2f
BarisSenel <122669318+BarisSenel@users.noreply.github.com>
2023-05-10 11:51:28 +0300

    Update SpamScript.lua

--- SpamScript.lua
+++ SpamScript.lua
@@ -1,39 +1,21 @@
-
-
+botposx = math.floor(getBot().x / 32)   -- Don't touch it
+botposy = math.floor(getBot().y / 32)   -- Don't touch it
+botstandx = 42 - 1   -- Client position - 1
+botstandy = 24 - 1   -- Client position - 1
+SpamWorld = "EXCSM"  -- Desired Spam World start it in spam world idk how to send it to exit :p
+spamtext = "SPAM TEXT HERE"  -- SPAM TEXT
+spamdelay = 10000 -- spam delay betwwen texts
 while true do
-spamtext = "HELLO"   
-botpositionx = 37 - 1   
-botpositiony = 21 - 1   
-SpamWorld = "DESIREDWORLD"
-
-if not getBot().world == SpamWorld then
+if getBot().world ~= SpamWorld:upper() then
     sleep(50000) --try to rejoin after 50 second you can adjust that 
-    sendPacket(3,"action|join_request\nname|SpamWorld\ninvitedWorld|0")
-
+    sendPacket(3,"action|join_request\nname|"..SpamWorld.."\ninvitedWorld|0")
 end
-
-
-
-if getBot().x == botpositionx * 32 and getBot().y == botpositiony * 32 then
-say(spamtext)
-sleep(10000) --spam speed
-
+if botposx == botstandx and botposy == botstandy then
+    say(spamtext)
+    sleep(spamdelay)
 else
-findPath(botpositionx,botpositiony)
-sleep(10000) --spam speed
-say(spamtext)
+    findPath(botstandx,botstandy)
+    say(spamtext)
+    sleep(spamdelay)
 end
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
